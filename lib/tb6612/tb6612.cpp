90af636a5bdf8da941e093d9ac0025cf42dfe828
rusty71 <jeroen@stylin.nl>
2016-10-22 11:37:56 +0200

    pins 10 & 12 cannot be used as pwm because of tachometer using timer1

--- lib/tb6612/tb6612.cpp
+++ lib/tb6612/tb6612.cpp
@@ -2,23 +2,23 @@
 
 //which pins connect to the tb6612
 const byte STBY = 13;	//Standby/Coast
-const byte PWMA = 12;	//Speed control. Must be an PWM enabled pin 
-const byte AIN1 = 10;	//control 1
+const byte PWMA = 3;	//Speed control. Must be an PWM enabled pin 
+const byte AIN1 = 12;	//control 1
 const byte AIN2 = 14;	//Control 2
 
 TB6612::TB6612(){
-    pinMode(STBY, OUTPUT);
+	pinMode(STBY, OUTPUT);
     pinMode(PWMA, OUTPUT);
-    pinMode(AIN1, OUTPUT);
-    pinMode(AIN2, OUTPUT);
-    analogWrite(PWMA, 0);	//set power to zero
+	pinMode(AIN1, OUTPUT);
+	pinMode(AIN2, OUTPUT);
+    analogWrite(PWMA, 100);	//set power to zero
 }
 
 TB6612::~TB6612(){/*nothing to destruct*/}
 
 //power 0-255
 void TB6612::move(int power, bool direction){
-	coast(0);
+	//coast(0);
 	if(direction){
 		digitalWrite(AIN1, LOW);
 		digitalWrite(AIN2, HIGH);
@@ -27,7 +27,6 @@ void TB6612::move(int power, bool direction){
 		digitalWrite(AIN1, HIGH);
 		digitalWrite(AIN2, LOW);
 	}
-	
 	analogWrite(PWMA, power);
 }
 
