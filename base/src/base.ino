511f5e9e3cc79f396d878851f539923eea470170
rusty71 <jeroen@stylin.nl>
2016-10-24 08:28:20 +0200

    async comms

--- base/src/base.ino
+++ base/src/base.ino
@@ -1,7 +1,40 @@
+/*
+ * This is the base software
+ * The base is responsible for all the RF communication with the cars
+ * The radios are half duplex. The base will continiously poll the cars
+ * for data and keep the link alive. The cars all have a unique address
+ * also a broadcast adress is available.
+ * The base also comminicates with the control software voer the serial
+ * port using CmdMessenger
+ * The base has very little knowledge about the application
+ */
+ 
 #include <SPI.h>
 #include <RH_RF69.h>
+#include <RHDatagram.h>
+#include <CmdMessenger.h>
 
+#define BASE_ADDRESS (0)
+#define CAR_ADDRESS (1)
+
+//setup radio
 RH_RF69 rf69(4, 2); // For RF69 on MoteinoMEGA
+//Unreliable addressable datagrams
+RHDatagram dgram(rf69, BASE_ADDRESS);
+
+
+//~ typedef enum state_t {
+						//~ WAITING,
+						//~ RESPONSE
+					 //~ };
+//~ state_t rfstate = WAITING;
+
+//host communication
+CmdMessenger c = CmdMessenger(Serial,',',';','/');
+
+enum {
+    seq_cmd,
+};
 
 #define CM_TICK (0.1832)
 typedef struct {
@@ -27,12 +60,13 @@ void setup()
 	uint8_t key[] = {	0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
 						0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08};
 	rf69.setEncryptionKey(key);			
+	dgram.setHeaderFrom(BASE_ADDRESS);
 }
 
 
 uint32_t last_ticks = 0;
 uint32_t last_millis = millis();
-void loop()
+void raceloop()
 {
 	uint8_t len = sizeof(comms);
 	if (rf69.available())
@@ -40,27 +74,36 @@ void loop()
 		if (rf69.recv((uint8_t*)(&comms), &len))
 		{
 			//RH_RF69::printBuffer("received: ", (uint8_t*)(&comms), len);
-			Serial.print(comms.segment,DEC);
-			Serial.print("\t");
-			Serial.print(comms.blade_pos,DEC);
-			Serial.print("\t");
-			Serial.print(((double)comms.xyz[0])/32.0);
-			Serial.print("\t");
-			Serial.print(((double)comms.xyz[1])/32.0);
-			Serial.print("\t");
-			Serial.print(((double)comms.xyz[2])/32.0);
-			Serial.print("\t");
-			Serial.print(comms.millis);
-			Serial.print("\t");
-			Serial.print(comms.millis-last_millis);
-			Serial.print("\t");
-			Serial.print(comms.ticks-last_ticks);
-			Serial.print("\t");
-			Serial.print((((double)(comms.millis-last_millis)/(double)(comms.ticks-last_ticks))*1000.0));
-			Serial.print("\t");
-			Serial.println(comms.ticks,DEC);
+			//~ Serial.print(comms.segment,DEC);
+			//~ Serial.print("\t");
+			//~ Serial.print(comms.blade_pos,DEC);
+			//~ Serial.print("\t");
+			//~ Serial.print(((double)comms.xyz[0])/32.0);
+			//~ Serial.print("\t");
+			//~ Serial.print(((double)comms.xyz[1])/32.0);
+			//~ Serial.print("\t");
+			//~ Serial.print(((double)comms.xyz[2])/32.0);
+			//~ Serial.print("\t");
+			//~ Serial.print(comms.millis);
+			//~ Serial.print("\t");
+			//~ Serial.print(comms.millis-last_millis);
+			//~ Serial.print("\t");
+			//~ Serial.print(comms.ticks-last_ticks);
+			//~ Serial.print("\t");
+			//~ Serial.print((((double)(comms.millis-last_millis)/(double)(comms.ticks-last_ticks))*1000.0));
+			//~ Serial.print("\t");
+			//~ Serial.println(comms.ticks,DEC);
 			last_ticks = comms.ticks;
 			last_millis = comms.millis;
+			c.sendCmdStart(seq_cmd);
+			c.sendCmdBinArg(comms.segment);
+			c.sendCmdBinArg(comms.blade_pos);
+			c.sendCmdBinArg(comms.xyz[0]);
+			c.sendCmdBinArg(comms.xyz[1]);
+			c.sendCmdBinArg(comms.xyz[2]);
+			c.sendCmdBinArg(comms.millis);
+			c.sendCmdBinArg(comms.ticks);
+			c.sendCmdEnd();		
 		}
 		else
 		{
@@ -69,3 +112,31 @@ void loop()
 	}
 }
 
+uint8_t send_buf[64];
+uint8_t recv_buf[64];
+uint8_t len, to, from, id, flags;
+
+uint8_t i=0;
+void loop()
+{
+	Serial.println("Sending..");
+	dgram.setHeaderId(i++);
+	//~ while(!dgram.waitPacketSent(1))
+		//~ ;
+	dgram.waitPacketSent();
+	dgram.sendto(send_buf, 20, RH_BROADCAST_ADDRESS);
+	if(dgram.waitAvailableTimeout(30)) { 	
+		if(dgram.recvfrom (recv_buf, &len, &from, &to, &id, &flags)) {
+			//~ Serial.print("Received message for : ");
+			//~ Serial.print(to);
+			//~ Serial.print(" from : ");
+			//~ Serial.print(from);
+			//~ Serial.print(" with id : ");
+			//~ Serial.print(id);
+			//~ Serial.print(" and flags : ");
+			//~ Serial.println(flags, BIN);
+		}
+	}
+	else
+		Serial.println("Timeout...");
+}
--- car/src/car.ino
+++ car/src/car.ino
@@ -1,15 +1,38 @@
 #include <SPI.h>
 #include <RH_RF69.h>
+#include <RHDatagram.h>
 #include <tb6612.h>
 #include <blade.h>
 #include <adxl345.h>
 #include <sfreed.h>
 #include <tachometer.h>
 
-#define CAR_SPEED (80)
+#define STANDALONE 1
+
+#ifdef STANDALONE
+#define CAR_SPEED (0)
+#define CAR_HIGH_SPEED (0)
+#else
+#define CAR_SPEED (100)
+#define CAR_HIGH_SPEED (200)
+#endif //STANALONE
+
+#define BASE_ADDRESS (0)
+#define CAR_ADDRESS (1)
+
+typedef enum state_t {
+						WAITING,
+						RESPONSE,
+						SENDING
+					 };
+state_t rfstate = WAITING;
+
 
 // Singleton instance of the radio driver
 RH_RF69 rf69(4, 2); // For MoteinoMEGA https://lowpowerlab.com/shop/moteinomega
+//Unreliable addressable datagrams
+RHDatagram dgram(rf69, CAR_ADDRESS);
+
 //Singleton motor driver 
 TB6612 Motor;
 //Singleton blade guide 
@@ -46,6 +69,8 @@ void setup()
 		Serial.println("setFrequency failed");
 	//~ // range 14 to 20. Too high doesn't mean better reception for short range
 	rf69.setTxPower(14);
+	dgram.setHeaderFrom(CAR_ADDRESS);
+
 	//encryption key
 	uint8_t key[] = { 	0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
 						0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08};
@@ -61,15 +86,18 @@ void setup()
 	//attach callback at every SF
 	sfreed.attach(SF_DETECT);
 
-	tacho.begin(18);		//every 18 ticks (~2mm/tick) increment segment position
+	tacho.begin(36);		//every 18 ticks (~2mm/tick) increment segment position
 
 	//current lap count (should be zero)
 	sfcount = sfreed.get();
 	//start car and wait for SF
 	Motor.move(CAR_SPEED,1);
 	//wait for start finish
+	
+#ifndef STANDALONE
 	while(sfcount == sfreed.get())
 		;
+#endif
 	//~ Serial.println("car started");
 	//~ Serial.println("start motor");
 }
@@ -83,10 +111,27 @@ typedef struct {
 } comms_t;
 comms_t comms;
 
-void loop()
+int16_t speed=CAR_SPEED;
+void loop_seq()
 {
+	
 	//collect data and send
 	comms.segment = tacho.get();
+	comms.segment = comms.segment*2;
+	if( ((comms.segment < 56) && (comms.segment > 35)) || ((comms.segment < 100) && (comms.segment > 80)) || ((comms.segment < 8) && (comms.segment > 0)))
+		speed = CAR_HIGH_SPEED;
+	else
+		speed = CAR_SPEED;
+
+	if((comms.segment < 63) && (comms.segment > 57))
+		Motor.brake(1);
+	else{
+		Motor.move(speed, 1);
+	}
+	comms.segment = tacho.get();
+	
+	//~ 40-60
+//~ 90-10
 	comms.blade_pos = blade.get();
 	adxl345.get(comms.xyz);
 	comms.ticks = tacho.getticks();
@@ -101,8 +146,104 @@ void loop()
 	//waitPacketSent() not needed?
 	rf69.waitPacketSent();
 
+#ifdef STANDALONE
+	if(tacho.get() >100){
+		tacho.sim(0);
+		SF_DETECT();
+	}
+	tacho.sim(tacho.get()+1);
+	delay(10);
+#endif
+
 	while(comms.segment == tacho.get());
 		;	//wait for next segment
 
+}
+
+
+//message queue ring buffer
+#define MAX_MSG_SIZE (64)
+#define MSG_BUF_SIZE (8)			//power of 2
+#define BUF_SIZE_MASK (0x07)		//power of 2
+uint8_t mesg_buf[MSG_BUF_SIZE][MAX_MSG_SIZE+1];	//first byte is length
+uint8_t head = 0, tail = 0;
+
+
+bool push_msg(uint8_t *msg, uint8_t len) {
+	if(((head + 1)&BUF_SIZE_MASK) == tail) {
+		Serial.println("Buffer full");
+		return false;	//buffer full
+	}
+	head = ((head + 1)&BUF_SIZE_MASK);
+	memcpy(&mesg_buf[head][1], msg, len);	
+	mesg_buf[head][0] = len;
+}
+
+uint8_t pop_msg(uint8_t *msg) {
+	uint8_t len;
+
+	if(head == tail)
+		return 0;
+	len = mesg_buf[tail][0];
+	memcpy(msg, &mesg_buf[tail][1], len);	
+	tail = (tail + 1)&BUF_SIZE_MASK;
+	return len;
+}
+
+//receive buffer and  parameters
+uint8_t send_buf[MAX_MSG_SIZE];
+uint8_t recv_buf[MAX_MSG_SIZE];
+uint8_t len, to, from, id, flags;
+uint8_t test_msg[5];	
+
+uint8_t i=0;
+void loop()
+{
+	dgram.setHeaderId(i++);
+	switch(rfstate) {
+		case WAITING:
+			if(dgram.waitAvailableTimeout(1)) {
+				len = MAX_MSG_SIZE;
+				if(dgram.recvfrom (recv_buf, &len, &from, &to, &id, &flags)) {
+				//~ if(dgram.recvfrom (recv_buf, &len, NULL, NULL, NULL, NULL)) {
+					//~ Serial.print("Received message for : ");
+					//~ Serial.print(to);
+					//~ Serial.print(" from : ");
+					//~ Serial.print(from);
+					//~ Serial.print(" with id : ");
+					//~ Serial.print(id);
+					//~ Serial.print(" and flags : ");
+					//~ Serial.println(flags, BIN);
+					rfstate = RESPONSE;		//response state
+				}
+				else {
+					Serial.println("Receive error");
+				}
+			}
+		break;
+		case RESPONSE:
+			//copy next message to send
+			len = pop_msg(send_buf);
+			Serial.print("message from queue :");
+			Serial.println(len);
+			rfstate = SENDING;		//send message
+		case SENDING:		//watch fallthrough from RESPONSE
+			if(dgram.waitPacketSent(1)) {	//ensure we can send without blocking FIXME:timeout = 0 fails
+				Serial.println("sending now");
+				dgram.sendto(send_buf, len, BASE_ADDRESS);
+				rfstate = WAITING;
+			}
+			else
+				Serial.println("cannot send, will block****************************************************************");
+		break;
+		default:
+			Serial.println("HELP");
+		break;
+	}
+	
+	//emulate burst sent
+	if(i>250)
+		push_msg(test_msg, 4);
+	//delay(1000);
 }
 
