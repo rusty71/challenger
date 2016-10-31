9040dc0a4b34981e2b926fec8270a7c856d6f3e3
rusty71 <jeroen@stylin.nl>
2016-10-31 22:43:40 +0100

    disable filtering, needs speedup

--- lib/adxl345/adxl345.cpp
+++ lib/adxl345/adxl345.cpp
@@ -46,48 +46,48 @@ void ADXL345::begin() {
 	writeTo(POWER_CTL, 0x08);
 }
 
-void ADXL345::get(int16_t *xyz){
-	int16_t x_min=0, x_max=0;
-	int16_t y_min=0, y_max=0;
-	int16_t z_min=0, z_max=0;
-	int16_t tmp;
-	int16_t x_sum = 0, y_sum=0, z_sum=0;
+//~ void ADXL345::get(int16_t *xyz){
+	//~ int16_t x_min=0, x_max=0;
+	//~ int16_t y_min=0, y_max=0;
+	//~ int16_t z_min=0, z_max=0;
+	//~ int16_t tmp;
+	//~ int16_t x_sum = 0, y_sum=0, z_sum=0;
 
-	for(byte i=0; i < 18; i++) {
-		//read x,y,z values from sensor in tmp array
-		readFrom( DATAX0, 6, _buff);
+	//~ for(byte i=0; i < 18; i++) {
+		//~ //read x,y,z values from sensor in tmp array
+		//~ readFrom( DATAX0, 6, _buff);
 
-		tmp = ((((int16_t)_buff[1]) << 8) | _buff[0]);
-		x_min = min(x_min, tmp);
-		x_max = max(x_max, tmp);
-		x_sum += tmp;
+		//~ tmp = ((((int16_t)_buff[1]) << 8) | _buff[0]);
+		//~ x_min = min(x_min, tmp);
+		//~ x_max = max(x_max, tmp);
+		//~ x_sum += tmp;
 
-		tmp = ((((int16_t)_buff[3]) << 8) | _buff[2]);
-		y_min = min(y_min, tmp);
-		y_max = max(y_max, tmp);
-		y_sum += tmp;
+		//~ tmp = ((((int16_t)_buff[3]) << 8) | _buff[2]);
+		//~ y_min = min(y_min, tmp);
+		//~ y_max = max(y_max, tmp);
+		//~ y_sum += tmp;
 
-		tmp = ((((int16_t)_buff[5]) << 8) | _buff[4]);
-		z_min = min(z_min, tmp);
-		z_max = max(z_max, tmp);
-		z_sum += tmp;
-	}
-	//remove minimum and maximum
-	x_sum = x_sum - x_min - x_max;
-	y_sum = y_sum - y_min - y_max;
-	z_sum = z_sum - z_min - z_max;
+		//~ tmp = ((((int16_t)_buff[5]) << 8) | _buff[4]);
+		//~ z_min = min(z_min, tmp);
+		//~ z_max = max(z_max, tmp);
+		//~ z_sum += tmp;
+	//~ }
+	//~ //remove minimum and maximum
+	//~ x_sum = x_sum - x_min - x_max;
+	//~ y_sum = y_sum - y_min - y_max;
+	//~ z_sum = z_sum - z_min - z_max;
 	
-	xyz[0] = (x_sum >> 4);
-	xyz[1] = (y_sum >> 4);
-	xyz[2] = (z_sum >> 4);
+	//~ xyz[0] = (x_sum >> 4);
+	//~ xyz[1] = (y_sum >> 4);
+	//~ xyz[2] = (z_sum >> 4);
 	
-}
+//~ }
 
 //returns x,y,z. Needs to be divided by 32.0, for 16G range, to get float values
-//~ void ADXL345::get(int16_t *xyz){
-	//~ readFrom( DATAX0, 6, _buff);
-	//~ //need to calibrate
-	//~ xyz[0] = ((((int16_t)_buff[1]) << 8) | _buff[0])-4;		//TODO fix static calibration
-	//~ xyz[1] = ((((int16_t)_buff[3]) << 8) | _buff[2])+3;
-	//~ xyz[2] = ((((int16_t)_buff[5]) << 8) | _buff[4])+4;
-//~ }
+void ADXL345::get(int16_t *xyz){
+	readFrom( DATAX0, 6, _buff);
+	//need to calibrate
+	xyz[0] = ((((int16_t)_buff[1]) << 8) | _buff[0])-4;		//TODO fix static calibration
+	xyz[1] = ((((int16_t)_buff[3]) << 8) | _buff[2])+3;
+	xyz[2] = ((((int16_t)_buff[5]) << 8) | _buff[4])+4;
+}
